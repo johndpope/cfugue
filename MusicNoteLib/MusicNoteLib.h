96c47133360d4557308459bee57e3df3a0cb5b64
krishnapg <krishnapg@ac09698f-bb87-48ee-8e36-73d3fdc553d8>
2009-05-04 12:09:26 +0000

    Initial Committ. Has Parser that works for Note root, velocity, octave

---
+++ MusicNoteLib/MusicNoteLib.h
@@ -0,0 +1,30 @@
+#ifndef __MUSICNOTE_LIB_H__EBEE094C_FF6E_43a1_A6CE_D619564F9C6A__
+#define __MUSICNOTE_LIB_H__EBEE094C_FF6E_43a1_A6CE_D619564F9C6A__
+
+/// <Summary>
+/// This header file can be included directly in your project or through
+/// MusicNoteLib.h of the MusicNoteDll project. If included directly, this
+/// will be built directly as a satic library. If included through MusicNoteDll
+/// this will use dllImports through MUSICNOTELIB_API
+/// </Summary>
+#ifndef MUSICNOTELIB_API
+#define MUSICNOTELIB_API
+#endif // MUSICNOTELIB_API
+
+#include "TChar.h"
+#include <string>
+
+namespace MusicNoteLib
+{
+	typedef TCHAR MusicChar;	
+
+extern "C"
+{
+    MUSICNOTELIB_API void* GetCarnaticMusicNoteReader();
+} // extern "C"
+
+
+} // namespace MusicNoteLib
+
+
+#endif // __MUSICNOTE_LIB_H__EBEE094C_FF6E_43a1_A6CE_D619564F9C6A__
