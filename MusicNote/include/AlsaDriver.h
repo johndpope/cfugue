7825116c0f3ebc16d8f44058ad6e5979aa88a8e0
krishnapg <krishnapg@ac09698f-bb87-48ee-8e36-73d3fdc553d8>
2011-03-02 03:20:29 +0000

    Writing AlsaDriver

---
+++ MusicNote/include/AlsaDriver.h
@@ -0,0 +1,59 @@
+#ifndef __ALSADRIVER_H__9857EC39_234E_411E_9558_EFDA218796AA__
+
+#include "rtmidi/RtMidi.h"
+#include "jdkmidi/driver.h"
+#include "jdkmidi/sequencer.h"
+
+  class MIDIDriverAlsa : public jdkmidi::MIDIDriver
+  {
+	  RtMidiIn*		m_pMidiIn;
+	  RtMidiOut*	m_pMidiOut;
+
+    public:
+      MIDIDriverAlsa ( int queue_size );
+      virtual ~MIDIDriverAlsa();
+      
+      void ResetMIDIOut();
+      
+      bool StartTimer ( int resolution_ms );
+      bool OpenMIDIInPort ( int id );
+
+      bool OpenMIDIOutPort ( int id );
+      
+      void StopTimer();
+      void CloseMIDIInPort();
+      void CloseMIDIOutPort();
+      
+      
+      bool HardwareMsgOut ( const MIDITimedBigMessage &msg );
+      
+    protected:
+    
+      static void CALLBACK win32_timer (
+        UINT wTimerID,
+        UINT msg,
+        DWORD_PTR dwUser,
+        DWORD_PTR dw1,
+        DWORD_PTR dw2
+      );
+      
+      static void CALLBACK win32_midi_in (
+        HMIDIIN hMidiIn,
+        UINT wMsg,
+        DWORD dwInstance,
+        DWORD dwParam1,
+        DWORD dwParam2
+      );
+      
+      //HMIDIIN in_handle;
+      //HMIDIOUT out_handle;
+      int timer_id;
+      int timer_res;
+      
+      //bool in_open;
+      //bool out_open;
+      bool timer_open;
+  };
+
+
+#endif // __ALSADRIVER_H__9857EC39_234E_411E_9558_EFDA218796AA__
---
+++ MusicNote/src/MusicNoteLib/MIDIDrivers/AlsaDriver.cpp
@@ -0,0 +1,98 @@
+/*
+	This is part of CFugue, a C++ Runtime for MIDI Score Programming
+	Copyright (C) 2011 Gopalakrishna Palem
+
+	For links to further information, or to contact the author,
+	see <http://musicnote.sourceforge.net/>.
+*/
+
+#include "AlsaDriver.h"
+
+using namespace jdkmidi;
+
+  MIDIDriverAlsa::MIDIDriverAlsa ( int queue_size )
+      :
+      MIDIDriver ( queue_size ),
+      m_pMidiIn ( 0 ),
+      m_pMidiOut ( 0 ),
+      //in_open ( false ),
+      //out_open ( false ),
+      timer_open ( false )
+  {
+  }
+  
+  MIDIDriverAlsa::~MIDIDriverAlsa()
+  {
+    StopTimer();
+    CloseMIDIInPort();
+    CloseMIDIOutPort();
+  }
+
+	bool MIDIDriverAlsa::OpenMIDIInPort ( int id )
+	{
+		if(m_pMidiIn != NULL) // close any open port
+		{
+			delete m_pMidiIn;
+			m_pMidiIn = NULL;
+		}
+		if(m_pMidiIn == NULL)
+		{
+			try
+			{
+				m_pMidiIn = new RtMidiIn("MIDIDriverAlsa Client");
+			}
+			catch ( RtError &error ) 
+			{
+				error.printMessage();			
+				return false;
+			}			
+		}
+		if(m_pMidiIn != NULL)
+		{
+			try
+			{
+				m_pMidiIn->openPort(id);
+			}
+			catch(RtError& error)
+			{
+				error.printMessage();
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	bool MIDIDriverAlsa::OpenMIDIOutPort ( int id )
+	{
+		if(m_pMidiOut != NULL) // close any open port
+		{
+			delete m_pMidiOut;
+			m_pMidiOut = NULL;
+		}
+		if(m_pMidiOut == NULL)
+		{
+			try
+			{
+				m_pMidiOut = new RtMidiOut("MIDIDriverAlsa Client");
+			}
+			catch(RtError &error)
+			{
+				error.printMessage();
+				return false;
+			}
+		}
+		if(m_pMidiOut != NULL)
+		{
+			try
+			{
+				m_pMidiOut->openPort(id);
+			}
+			catch(RtError &error)
+			{
+				error.printMessage();
+				return false;
+			}
+		}
+		return true;
+	}
