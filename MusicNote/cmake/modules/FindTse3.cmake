ab3225415a480f292e2d944d14d1267ada361ca8
krishnapg <krishnapg@ac09698f-bb87-48ee-8e36-73d3fdc553d8>
2012-09-02 04:20:00 +0000

    FindTse3.cmake working - but tests are including qtVUMeter.cpp multiple times, causing problem

--- MusicNote/cmake/modules/FindTse3.cmake
+++ MusicNote/cmake/modules/FindTse3.cmake
@@ -10,19 +10,17 @@
 # Redistribution and use is allowed according to the terms of CarMusTy license.
 # Refer to http://CarMusTy.SourceForge.net
 
-FIND_PATH(TE3_INCLUDE_DIR TSE3.h)
+FIND_PATH(TSE3_INCLUDE_DIR tse3/TSE3.h     
+	PATHS
+    /usr/include)
 
 SET(TSE3LIB_NAMES ${TSE3LIB_NAMES} tse3 libtse3)
 FIND_LIBRARY(TSE3_LIBRARY NAMES ${TSE3LIB_NAMES} PATHS /usr/lib /usr/local/lib /opt/libtse3/lib)
 
-# handle the QUIETLY and REQUIRED arguments and set TSE3_FOUND to TRUE if 
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs.cmake)
+# handle the QUIETLY and REQUIRED arguments and set TSE3_FOUND to TRUE if all listed variables are TRUE
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(TSE3  DEFAULT_MSG  TSE3_LIBRARY  TSE3_INCLUDE_DIR)
 
-IF(TSE3_FOUND)
-  SET( TSE3_INCLUDE_DIRS ${TE3_INCLUDE_DIR})
-  SET( TSE3_LIBRARIES ${TSE3_LIBRARY} )
-ENDIF(TSE3_FOUND)
+SET(TSE3_INCLUDE_DIRS ${TE3_INCLUDE_DIR})
+SET(TSE3_LIBRARIES ${TSE3_LIBRARY})
 
-MARK_AS_ADVANCED(TSE3_INCLUDE_DIRS TSE3_LIBRARIES)
+# MARK_AS_ADVANCED(TSE3_INCLUDE_DIR TSE3_LIBRARY)
